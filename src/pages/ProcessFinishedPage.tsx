73c2267c1221096156425a837d8362969b1948b7
pallascodes <pallascodes@gmail.com>
2025-07-17 17:59:36 -0600

    fix: hardcoded publicUrl removed

--- src/pages/ProcessFinishedPage.tsx
+++ src/pages/ProcessFinishedPage.tsx
@@ -1,14 +1,24 @@
+import { ErrorMessage } from '@/components/ErrorMessage'
+import { useAuth } from '@/store/auth.store'
+
 export default function ProcessFinishedPage() {
+  const { solDom } = useAuth()
+
+  if (!solDom || !solDom.publicUrl) {
+    return (
+      <ErrorMessage
+        title="Error al obtener el documento"
+        description="Vuelve a abrir el enlace que se envío a tu celular."
+      />
+    )
+  }
+
   return (
     <div className="text-center">
       <h2 className="font-bold antialiased text-xl mt-4 mb-2">Proceso finalizado</h2>
       <p>
         Visualiza tu documento de domiciliación dando&nbsp;
-        <a
-          href="https://s3.amazonaws.com/gbplus.inter3.ordenes/2025/700046/700046.4239.1752773273541.pdf"
-          target="_blank"
-          className="font-medium text-blue-600"
-        >
+        <a href={solDom.publicUrl} target="_blank" className="font-medium text-blue-600">
           click aquí
         </a>
       </p>
